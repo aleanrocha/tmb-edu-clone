21cc4112d32523f9e89c1e9044eff2be7095f3ba
aleanrocha <aleanrocha.dev@gmail.com>
2024-10-30 18:11:44 -0300

    fix routing configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Home } from './screens/Home'
+
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,27 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Home } from '../screens/Home'
-import { Login } from '../screens/Home'
-import { ForgotPassword } from '../screens/Home'
-import { Producer } from '../screens/Home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-    children: [
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'recuperarSenha',
-        element: <ForgotPassword />,
-      },
-      {
-        path: 'produtor',
-        element: <Producer />,
-      },
-    ],
-  },
+  }
 ])
